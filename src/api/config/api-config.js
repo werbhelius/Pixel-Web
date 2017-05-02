6d3452359d52f6f90857a23a1fca7b91881e5e7d
wanbo <wanbowb@live.com>
2017-05-02 22:57:48 +0800

    add key config

--- src/api/config/api-config.js
+++ src/api/config/api-config.js
@@ -1,6 +1,11 @@
+const key_config = {
+
+}
+
 const host_config = {
     host: 'https://api.weibo.com/2/',
     oauth: 'https://api.weibo.com/oauth2/authorize'
 }
 
 export const HOST_CONCIG = host_config;
+export const KEY_CONFIG = key_config
