3fa4241e0be56d2974cdf61cc688f4397d41cb9a
wanbo <wanbowb@live.com>
2017-05-24 20:28:08 +0800

    fix image click position

--- src/components/ImageZoom.vue
+++ src/components/ImageZoom.vue
@@ -1,10 +1,10 @@
 <template lang="html">
-    <div class="image-zoom">
+    <div id="zoomDiv" class="image-zoom">
         <div class="image-close" v-on:click="goBack">
             <svg viewBox="0 0 46 72" style="display: inline-block; fill: currentcolor; height: 100%; width: 100%; position: relative; user-select: none; vertical-align: text-bottom;"><g><path d="M27.243 36l14.879-14.879a2.998 2.998 0 0 0 0-4.242 2.998 2.998 0 0 0-4.242 0L23 31.758 8.122 16.879a2.998 2.998 0 0 0-4.242 0 2.998 2.998 0 0 0 0 4.242L18.758 36 3.879 50.879A2.998 2.998 0 0 0 6.001 56a2.99 2.99 0 0 0 2.121-.879L23 40.242l14.879 14.879A2.991 2.991 0 0 0 40 56a2.998 2.998 0 0 0 2.121-5.121L27.243 36z"></path></g></svg>
         </div>
-        <div class="image-div">
-            <img class="img" :src="image_zoom_url" alt="">
+        <div class="image-div" :class="over?'tran':''" >
+            <img id="img" class="img" :src="image_zoom_url">
         </div>
     </div>
 </template>
@@ -15,10 +15,28 @@ import { mapGetters, mapActions } from 'vuex'
 
 export default {
     name: "image-zoom",
+    data() {
+        return {
+            over: false
+        }
+    },
     computed: {
         ...mapGetters({
             image_zoom_url: 'image_zoom_url'
         }),
+    },
+    mounted() {
+        var vue = this
+        var img = document.getElementById('img')
+        var zoom = document.getElementById('zoomDiv')
+        img.onload = function () {
+            if (img.offsetHeight > zoom.offsetHeight) {
+                vue.over = true
+            } else {
+                vue.over = false
+            }
+        }
+
     },
     methods: {
         ...mapActions([
@@ -32,7 +50,7 @@ export default {
 }
 </script>
  
-<style lang="css">
+<style lang="css" scoped>
 .image-zoom {
     width: 100%;
     height: 100%;
@@ -64,4 +82,8 @@ export default {
     -webkit-overflow-scrolling: touch;
 }
 
+.image-zoom .tran {
+    top: 0 ;
+    transform: translateY(0) ;
+}
 </style>
--- src/views/ImageZoom/index.js
+++
@@ -1,7 +0,0 @@
-import ImageZoom from './src/ImageZoom'
-
-ImageZoom.install = function (Vue) {
-    Vue.component(ImageZoom.name, ImageZoom)
-}
-
-export default ImageZoom
